948abc3500f45578a386182c0d2a55bcfcbc2c66
Florian Knodt <adlerweb@adlerweb.info>
2020-02-28 23:26:34 +0100

    Add missing backend header. Fixes #11.

--- src/common/CupsPrintEnvironment.h
+++ src/common/CupsPrintEnvironment.h
@@ -21,6 +21,8 @@
 #ifndef h952b1c81_8931_433a_8479_7ae6d8e85a86
 #define h952b1c81_8931_433a_8479_7ae6d8e85a86
 
+#include <cups/backend.h>
+#include <cups/sidechannel.h>
 #include "PrinterDriver.h"
 
 namespace DymoPrinterDriver
